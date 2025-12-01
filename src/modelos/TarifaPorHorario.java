111e439d1c311418d75fa5b398c19a69aed33e86
Guille_Portillo <guillepdu03@gmail.com>
2025-12-01 16:59:50 -0300

    Se modifica TartifasPorHorario a su version final (Segun creemos)

--- src/modelos/TarifaPorHorario.java
+++ src/modelos/TarifaPorHorario.java
@@ -1,17 +1,26 @@
 package src.modelos;
 
 import src.interfaces.Tarifa;
+import java.time.LocalDateTime;
 
 public class TarifaPorHorario implements Tarifa {
-
     private final double[][] matrizTarifas = {
-            {0, 0, 0},
-            {0, 0, 0},
-            {0, 0, 0}
+            {100, 100, 150},  // Moto
+            {130, 130, 150},  // Auto
+            {130, 130, 150}   // Camioneta
     };
 
-    @Override
+
     public double calcularTarifa(long minutos, int tipoVehiculo) {
-        return 0; // Implementado luego
+
+        int hora = LocalDateTime.now().getHour();
+        int horario;
+
+        if (hora >= 6 && hora < 12) horario = 0;
+        else if (hora >= 12 && hora < 18) horario = 1;
+        else horario = 2;
+
+        double precioPorMinuto = matrizTarifas[tipoVehiculo][horario];
+        return minutos * precioPorMinuto;
     }
 }
