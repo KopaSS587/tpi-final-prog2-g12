17d43d9f994db2b41f70956443c7b15356e32f9e
Guille_Portillo <guillepdu03@gmail.com>
2025-12-01 16:52:11 -0300

    Se modifico la clase ticket

--- src/modelos/Ticket.java
+++ src/modelos/Ticket.java
@@ -1,4 +1,33 @@
 package src.modelos;
 
+import java.time.LocalDateTime;
+
 public class Ticket {
+
+    private Vehiculo vehiculo;
+    private LocalDateTime salida;
+    private double monto;
+
+    public Ticket(Vehiculo vehiculo) {
+        this.vehiculo = vehiculo;
+    }
+
+    public Vehiculo getVehiculo() { return vehiculo; }
+    public LocalDateTime getSalida() { return salida; }
+    public double getMonto() { return monto; }
+
+    public void registrarSalida(LocalDateTime salida) {
+        this.salida = salida;
+    }
+
+    public void setMonto(double monto) {
+        this.monto = monto;
+    }
+
+    public long calcularMinutos() {
+        return java.time.Duration.between(
+                vehiculo.getHoraEntrada(),
+                salida
+        ).toMinutes();
+    }
 }
