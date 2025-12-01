a57c34e39c9f70361558e1f09c82eb02651489c1
Guille_Portillo <guillepdu03@gmail.com>
2025-12-01 16:24:56 -0300

    Se modifica la clase TarifaPorHorario

--- src/modelos/TarifaPorHorario.java
+++ src/modelos/TarifaPorHorario.java
@@ -1,4 +1,17 @@
 package src.modelos;
 
-public class TarifaPorHorario {
+import src.interfaces.Tarifa;
+
+public class TarifaPorHorario implements Tarifa {
+
+    private final double[][] matrizTarifas = {
+            {0, 0, 0},
+            {0, 0, 0},
+            {0, 0, 0}
+    };
+
+    @Override
+    public double calcularTarifa(long minutos, int tipoVehiculo) {
+        return 0; // Implementado luego
+    }
 }
